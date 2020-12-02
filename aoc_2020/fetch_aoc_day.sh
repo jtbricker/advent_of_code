c96902c8a79c666fd1fac2c99e585beef338b3b7
Justin <justin.bricker@eventbrite.com>
2020-12-02 11:32:24 -0600

    Update fetch_aoc_day-sh to use solution template

---
+++ aoc_2020/fetch_aoc_day.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make sure to set $AOC_SESSION_COOKIE in ENV vars
+
+DAY=$(printf "%02d" $1)
+cp -R day_XX day_$DAY
+curl --cookie session=$AOC_SESSION_COOKIE -o ./day_$DAY/input.txt https://adventofcode.com/2020/day/$1/input
+code .
+sleep 2
+open -a "Google Chrome" https://adventofcode.com/2020/day/$1
