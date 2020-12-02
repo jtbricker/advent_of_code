9991bf4bc8cfdef95461bcf2e8f7eb0aa80549cb
Justin <justin.bricker@eventbrite.com>
2020-12-02 11:31:58 -0600

    Add solution template

---
+++ aoc_2020/day_XX/solution.py
@@ -0,0 +1,13 @@
+from lib.helpers import load_input
+
+def main():
+    data = load_input(__file__)
+
+    # Remove this
+    for d in data:
+        print(d)
+    
+    # Write Solution here
+
+if __name__ == "__main__":
+    main()
