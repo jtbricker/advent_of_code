674812c130f9cb83885d39f3ad4bad12b05d4ba0
Justin <justin.bricker@eventbrite.com>
2020-12-02 11:33:40 -0600

    Add run.py to run arbitrary solution

---
+++ aoc_2020/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import importlib
+
+
+def main(which_days):
+    for day in which_days:
+        try:
+            day_module = importlib.import_module(
+                'day_{0:02d}'.format(day))
+            print("Solutions to Day {0:02d}\n-------------------".format(day))
+            day_module.main()
+            print('')
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("Advent of Code AOC2020")
+    parser.add_argument(
+        'day', nargs='*', default=None, help="Run only specific day's problem")
+    args = parser.parse_args()
+
+    if args.day:
+        days = map(int, args.day)
+    else:
+        days = range(1, 26)
+
+    main(days)
