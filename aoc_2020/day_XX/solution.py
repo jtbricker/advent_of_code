4ea9be5b42978552a7e6414c8740b5cbb8e1a768
Justin <justin.bricker@eventbrite.com>
2020-12-03 11:00:19 -0600

    Update fetch code and solution template

--- aoc_2020/day_XX/solution.py
+++ aoc_2020/day_XX/solution.py
@@ -1,13 +1,30 @@
 from lib.helpers import load_input
 
+def part_1(data):
+    """[summary]
+
+    Args:
+        data ([type]): [description]
+    """
+    return 
+
+def part_2(data):
+    """[summary]
+
+    Args:
+        data ([type]): [description]
+    """
+
 def main():
     data = load_input(__file__)
 
-    # Remove this
+    # Remove this #########
     for d in data:
         print(d)
-    
-    # Write Solution here
+    #######################
+
+    print(part_1(data))
+    print(part_2(data))
 
 if __name__ == "__main__":
     main()
--- aoc_2020/fetch_aoc_day.sh
+++ aoc_2020/fetch_aoc_day.sh
@@ -5,6 +5,6 @@
 DAY=$(printf "%02d" $1)
 cp -R day_XX day_$DAY
 curl --cookie session=$AOC_SESSION_COOKIE -o ./day_$DAY/input.txt https://adventofcode.com/2020/day/$1/input
-code .
+code /Users/justin.bricker/eventbrite/my/advent_of_code/
 sleep 2
 open -a "Google Chrome" https://adventofcode.com/2020/day/$1
