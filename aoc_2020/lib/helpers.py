9339c3435d37d900fa8152bb91aa062f32fc7fe9
Justin <justin.bricker@eventbrite.com>
2020-12-08 11:31:35 -0600

    Add missing lib files, remove from .gitignore for now

---
+++ aoc_2020/lib/helpers.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+def load_input(file: str):
+    input_path = Path(file).parent.absolute() / 'input.txt'
+
+    with input_path.open('r') as file:
+        return [line.strip() for line in file.readlines() if line.strip() != '']
+
+def load_input_chunked(file: str):
+    input_path = Path(file).parent.absolute() / 'input.txt'
+
+    with input_path.open('r') as file:
+        lines = file.readlines()
+    
+    data = []
+    chunk = []
+    for line in lines:
+        if line.strip() == '':
+            data.append(chunk)
+            chunk = []
+        else:
+            chunk.append(line.strip())
+    if chunk not in data:
+        data.append(chunk)
+    return data
