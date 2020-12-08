982505221fcf48b0d2c348f34d08fcff2017dd0a
Justin <justin.bricker@eventbrite.com>
2020-12-08 13:20:35 -0600

    Accept input_file as optional 2nd param to enable test inputs

--- aoc_2020/lib/helpers.py
+++ aoc_2020/lib/helpers.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-def load_input(file: str):
-    input_path = Path(file).parent.absolute() / 'input.txt'
+def load_input(file: str, input_file_name:str='input.txt'):
+    input_path = Path(file).parent.absolute() / input_file_name
 
     with input_path.open('r') as file:
         return [line.strip() for line in file.readlines() if line.strip() != '']
 
-def load_input_chunked(file: str):
-    input_path = Path(file).parent.absolute() / 'input.txt'
+def load_input_chunked(file: str, input_file_name:str='input.txt'):
+    input_path = Path(file).parent.absolute() / input_file_name
 
     with input_path.open('r') as file:
         lines = file.readlines()
