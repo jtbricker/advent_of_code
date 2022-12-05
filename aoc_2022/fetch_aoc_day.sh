0c96fcc9bb1327d37d1390b2c9061dd0cb9db026
Justin Bricker <jt.bricker@gmail.com>
2022-12-05 12:03:35 -0600

    Notes on cp command

--- aoc_2022/fetch_aoc_day.sh
+++ aoc_2022/fetch_aoc_day.sh
@@ -4,6 +4,7 @@
 export AOC_SESSION_COOKIE="53616c7465645f5f9b42f17f21690b471e6ec2a64d8d7dc4cd788436926ca010b6865bceb5b48ed567af57b792525a8efb4f180b001fae2182089db166a4a7ef"
 
 DAY=$(printf "%02d" $1)
+# TODO: This cp produces unexpected behavior if `day_$DAY` folder already exists
 cp -R day_XX day_$DAY
 curl --cookie session=$AOC_SESSION_COOKIE -o ./day_$DAY/input.txt https://adventofcode.com/2022/day/$1/input
 code ..
