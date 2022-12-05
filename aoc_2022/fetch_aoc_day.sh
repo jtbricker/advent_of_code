6eba68e4f88816eb8715f38709affa549a19fb26
Justin Bricker <jt.bricker@gmail.com>
2022-12-05 02:51:17 -0600

    Initialize AOC 2022

---
+++ aoc_2022/fetch_aoc_day.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure to set $AOC_SESSION_COOKIE in ENV vars
+export AOC_SESSION_COOKIE="53616c7465645f5f9b42f17f21690b471e6ec2a64d8d7dc4cd788436926ca010b6865bceb5b48ed567af57b792525a8efb4f180b001fae2182089db166a4a7ef"
+
+DAY=$(printf "%02d" $1)
+cp -R day_XX day_$DAY
+curl --cookie session=$AOC_SESSION_COOKIE -o ./day_$DAY/input.txt https://adventofcode.com/2022/day/$1/input
+code /Users/justin.bricker/eventbrite/my/advent_of_code/
+sleep 2
+open -a "Google Chrome" https://adventofcode.com/2022/day/$1
---
+++ aoc_2022/run.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+import importlib
+
+
+def main(which_days):
+    for day in which_days:
+        day_module = importlib.import_module(
+            'day_{0:02d}'.format(day))
+        print("Solutions to Day {0:02d}\n-------------------".format(day))
+        day_module.main()
+        print('')
+
+def test(day):
+    test_module = importlib.import_module('day_{0:02d}.test_solution'.format(day))
+    print("Tests for Day {0:02d}\n-------------------".format(day))
+    test_module.run_tests()
+    print('')
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("Advent of Code AOC2022")
+    parser.add_argument(
+        'day', nargs=1, default=None, help="Run only specific day's problem")
+    parser.add_argument(
+        '--test', '-t', action='store_true', help="Run test for day")
+    args = parser.parse_args()
+
+    if args.day:
+        days = map(int, args.day)
+    else:
+        days = range(1, 26)
+
+    if args.test:
+        day = list(days)[0]
+        test(day)
+    
+    else:
+        main(days)
