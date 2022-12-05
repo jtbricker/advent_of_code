78c115220a3e26b091dc7f9d96a2d7bc6782df36
Justin Bricker <jt.bricker@gmail.com>
2022-12-05 11:50:55 -0600

    Fix fetch script

--- aoc_2022/fetch_aoc_day.sh
+++ aoc_2022/fetch_aoc_day.sh
@@ -6,6 +6,6 @@ export AOC_SESSION_COOKIE="53616c7465645f5f9b42f17f21690b471e6ec2a64d8d7dc4cd788
 DAY=$(printf "%02d" $1)
 cp -R day_XX day_$DAY
 curl --cookie session=$AOC_SESSION_COOKIE -o ./day_$DAY/input.txt https://adventofcode.com/2022/day/$1/input
-code /Users/justin.bricker/eventbrite/my/advent_of_code/
+code ..
 sleep 2
 open -a "Google Chrome" https://adventofcode.com/2022/day/$1
